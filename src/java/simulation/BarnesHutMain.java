ae92d93dfc3b62929cb00400a5a8b6cb2cd98d96
Manos Chatzakis <chatzakis@ics.forth.gr>
2021-05-25 13:58:40 +0300

    added readme

--- src/java/simulation/BarnesHutMain.java
+++ src/java/simulation/BarnesHutMain.java
@@ -1,10 +1,33 @@
 package simulation;
 
-public class BarnesHutMain{
+import java.util.ArrayList;
+import structures.*;
 
-    public static void main(String [] args){
-        System.out.println("Hello!");
+public class BarnesHutMain {
+
+  public static void main(String[] args) {
+    String filename = args[0];
+    int iters = Integer.parseInt(args[1]);
+    int threads = Integer.parseInt(args[2]);
+
+    System.out.println("Filename: " + filename);
+    System.out.println("Iterations: " + iters);
+    System.out.println("Threads: " + threads);
+
+    ArrayList<Entity> entities = new ArrayList<>();
+    double dims = BHUtils.parseFile(filename, entities);
+
+    long start = System.currentTimeMillis();
+
+    if (threads == 0) {
+      //seq
+    } else {
+      //par
     }
+
+    long finish = System.currentTimeMillis();
+    long timeElapsed = finish - start;
+
+    System.out.println("Execution time: " + timeElapsed);
+  }
 }
-
-
